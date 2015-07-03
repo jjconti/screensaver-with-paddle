35d370f8f76e66afeaea2ef08bfe72914bfc6228
Juanjo Conti <jjconti@carouselapps.com>
2015-07-03 03:37:42 -0300

    Initial commit

---
+++ Ej/Ej/EjView.h
@@ -0,0 +1,13 @@
+//
+//  EjView.h
+//  Ej
+//
+//  Created by Juanjo Conti on 3/7/15.
+//  Copyright (c) 2015 CarouselApps. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface EjView : ScreenSaverView
+
+@end
---
+++ Ej/Ej/EjView.m
@@ -0,0 +1,52 @@
+//
+//  EjView.m
+//  Ej
+//
+//  Created by Juanjo Conti on 3/7/15.
+//  Copyright (c) 2015 CarouselApps. All rights reserved.
+//
+
+#import "EjView.h"
+
+@implementation EjView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
